850ca17ab0513d394f5190f313e5e7091e0fba21
shubhansu-kr <shubhansu2021@gmail.com>
2022-08-18 23:25:35 +0530

    BFS - Breadth First Search

--- 02_BreadthFirstSearch.cpp
+++ 02_BreadthFirstSearch.cpp
@@ -3,6 +3,29 @@
 #include <bits/stdc++.h>
 using namespace std ;
 
+class Solution1 {
+public:
+    vector<int> BFS(int stNode, vector<vector<int>> &list) {
+        queue<int> q; 
+        q.push(stNode);
+
+        int n = list.size();
+        vector<int> v(n, 0), bfs;
+        v[stNode] = 1;
+
+        while (q.size()) {
+            int node = q.front();
+            bfs.emplace_back(node);
+
+            for (auto &it: list[node]) {
+                if (!v[it]) q.push(it), v[it] = 1;
+            }
+            q.pop();
+        }      
+        return bfs;   
+    }
+};
+
 class Solution {
 public:
     void BFS(int stNode, vector<vector<int>> &list) {
