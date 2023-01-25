ffbead6ca2539ea509af9c8c026454efba0d7ffd
Shubhansu kumar <shubhansu2021@gmail.com>
2023-01-25 22:57:00 +0530

    Find cycles in the graph
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- 09_DetectCycleI.cpp
+++ 09_DetectCycleI.cpp
@@ -4,11 +4,36 @@
 using namespace std;
 
 class Solution
+{    
+public:
+    bool isCycle(int V, vector<int> adj[])
+    {
+        vector<int> visited(V);
+        for (int i = 0; i < V; ++i)
+        {
+            if (!visited[i] && dfs(adj, visited, -1, i)) return true; 
+        }
+        return false;
+    }
+
+    bool dfs(vector<int> adj[], vector<int> &visited, int prev, int i){
+        if (visited[i]) return true;
+
+        visited[i] = 1;
+        for(auto &it: adj[i]) {if (it != prev && dfs(adj, visited, i, it)) return true;} 
+    
+        return false;
+    }
+};
+
+class Solution
 {
     // Wrong Answer: 
     // 4 2
     // 1 2
     // 2 3
+
+    // Constraint the previous state.
 public:
     bool isCycle(int V, vector<int> adj[])
     {
