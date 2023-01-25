008b66b500ae20810a58ecc5c434e2bce4da4fde
Shubhansu kumar <shubhansu2021@gmail.com>
2023-01-25 22:51:41 +0530

    Wrong answer
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

---
+++ 09_DetectCycleI.cpp
@@ -0,0 +1,37 @@
+// https://practice.geeksforgeeks.org/problems/detect-cycle-in-an-undirected-graph/1
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+    // Wrong Answer: 
+    // 4 2
+    // 1 2
+    // 2 3
+public:
+    bool isCycle(int V, vector<int> adj[])
+    {
+        vector<int> visited(V);
+        for (int i = 0; i < V; ++i)
+        {
+            if (!visited[i] && dfs(adj, visited, i)) return true; 
+        }
+        return false;
+    }
+
+    bool dfs(vector<int> adj[], vector<int> &visited, int i){
+        if (visited[i]) return true;
+
+        visited[i] = 1;
+        for(auto &it: adj[i]) if (dfs(adj, visited, it)) return true;
+    
+        return false;
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
