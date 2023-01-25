f5f10ac68a064b252af3f235f899cca072be7d98
Shubhansu kumar <shubhansu2021@gmail.com>
2023-01-25 21:47:09 +0530

    Created Templates
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- 08_DetectCycle.cpp
+++ 08_DetectCycle.cpp
@@ -4,6 +4,76 @@
 using namespace std ;
 
 class Solution {
+    // Create Templates.
+public:
+    vector<vector<int>> createAdjMatrix(vector<vector<int>> &edges, int &n){
+        vector<vector<int>> adj(n+1, vector<int>(n+1, 0));
+        for(auto &it: edges){
+            // undirected graph
+            adj[it[0]][it[1]] = 1;
+            adj[it[1]][it[0]] = 1;
+        }
+        return adj;
+    }
+
+    vector<vector<int>> createAdjList(vector<vector<int>> &edges, int &n){
+        vector<vector<int>> adj(n+1);
+        for(auto &it: edges){
+            // udirected graph
+            adj[it[0]].emplace_back(it[1]);
+            adj[it[1]].emplace_back(it[0]);
+        }
+        return adj;
+    }
+
+    vector<int> DFS(vector<vector<int>> &list, int stNode)
+    {
+        vector<int> visited(list.size(), 0), traversal;
+        solveDFS(visited, traversal, list, stNode);
+        return traversal;
+    }
+    void solveDFS(vector<int> &visited, vector<int> &traversal, vector<vector<int>> &list, int node)
+    {
+    
+        traversal.emplace_back(node);
+        visited[node] = 1;
+    
+        for (int i = 0; i < list[node].size(); ++i)
+        {
+            if (visited[list[node][i]])
+                continue;
+            else
+                solveDFS(visited, traversal, list, list[node][i]);
+        }
+    }
+
+    vector<int> BFS(int stNode, vector<vector<int>> &list) {
+        queue<int> q;
+        q.push(stNode);
+    
+        int n = list.size();
+        vector<int> v(n, 0), bfs;
+        v[stNode] = 1;
+    
+        while (q.size()) {
+            int node = q.front();
+            bfs.emplace_back(node);
+    
+            for (auto &it: list[node]) {
+                if (!v[it]) q.push(it), v[it] = 1;
+            }
+            q.pop();
+        }
+        return bfs;
+    }
+
+    
+    bool containsCycle(vector<vector<char>>& grid) {
+        
+    }
+};
+
+class Solution {
     // Wrong Answer: 
     // [
     //   ["b","a","c"],
@@ -11,6 +81,8 @@ class Solution {
     //   ["d","d","c"],
     //   ["b","c","c"]
     // ]
+
+    // We are not handling the cases properly: 
 public:
     bool containsCycle(vector<vector<char>>& grid) {
         int m = grid.size(), n = grid[0].size();
@@ -20,23 +92,23 @@ public:
         {
             for (int j = 0; j < n; ++j)
             {
-                if (!visited[i][j] && dfs(grid, visited, grid[i][j], i, j)) return true;
+                if (!visited[i][j] && dfs(grid, visited, i, j)) return true;
             }
         }
         
         return false;
     }
 
-    bool dfs(vector<vector<char>> &grid, vector<vector<int>> &visited, char &val, int i, int j){
+    bool dfs(vector<vector<char>> &grid, vector<vector<int>> &visited, int i, int j){
 
-        if (visited[i][j] == val) return true;
-        else visited[i][j] = val;
+        if (visited[i][j] == grid[i][j]) return true;
+        else visited[i][j] = grid[i][j];
 
         int m = grid.size(), n = grid[0].size();
         // Go down
-        if (i < m-1 && grid[i][j] == grid[i+1][j] && dfs(grid, visited, val, i+1, j)) return true; 
+        if (i < m-1 && grid[i][j] == grid[i+1][j] && dfs(grid, visited, i+1, j)) return true; 
         // Go right
-        if (j < n-1 && grid[i][j] == grid[i][j+1] && dfs(grid, visited, val, i, j+1)) return true; 
+        if (j < n-1 && grid[i][j] == grid[i][j+1] && dfs(grid, visited, i, j+1)) return true; 
 
         return false;
     }
