564d4085b31e4dbe44dea8d068dca1deefbba3b7
Shubhansu kumar <shubhansu2021@gmail.com>
2023-01-27 22:14:40 +0530

    Solution1: USE BFS to detect cycle
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- 09_DetectCycleI.cpp
+++ 09_DetectCycleI.cpp
@@ -3,73 +3,113 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution1
-{   
-    // Segmentation Fault: RE
-    // BFS: Solution with Queue.
-public:
+class Solution1 {
+    // BFS solution with queue. 
+    // Use pairs to store the prev/origin node
+public: 
     bool isCycle(int V, vector<vector<int>> &adj){
         vector<int> visited(V+1);
+        queue<pair<int, int>> q;
 
         // Run through each distinct connections.
         for (int i = 1; i <= V; ++i)
         {  
             if (visited[i]) continue;
 
-            queue<int> q;
-            int prev = -1, cur;
-            q.push(i); // unvisited node
+            int cur, prev;
+            q.push({i, -1}); // unvisited node
 
             while(q.size()){
-                cur = q.front();
+                cur = q.front().first;
+                prev = q.front().second;
+
                 q.pop();
 
                 visited[cur] = 1;
 
                 for(auto &it: adj[cur]){
-                    if (it == prev) {
-                        // skip
-                    }
+                    if (it == prev) {continue;}
                     else {
                         if (visited[it]) return true;
-                        else q.push(it);
+                        else q.push({it, cur});
                     }
                 }
-                prev = cur;
             }
         }
         return false;
     }
 };
 
-class Solution
-{
-    // DFS: Solution with stack: WA: How to track the previous state.
-public:
-    bool isCycle(int V, vector<int> adj[])
-    {
-        vector<int> visited(V);
-        stack<int> st;
-        for (int i = 0; i < V; ++i)
-        {
-            if (!visited[i])
-            {
-                st.push(i);
-                while (st.size())
-                {
-                    int top = st.top();
-                    st.pop();
 
-                    if (visited[top]) return true;
-                    visited[top] = 1;
+// ERROR: LOGIC: When two nodes are connected to one node, we loose the prev track. 
+// class Solution
+// {   
+//     // Segmentation Fault: RE
+//     // BFS: Solution with Queue.
+// public:
+//     bool isCycle(int V, vector<vector<int>> &adj){
+//         vector<int> visited(V+1);
 
-                    for (auto &it : adj[top]) {st.push(it);}
-                }
-            }
-        }
-        return false;
-    }
-};
+//         // Run through each distinct connections.
+//         for (int i = 1; i <= V; ++i)
+//         {  
+//             if (visited[i]) continue;
+
+//             queue<int> q;
+//             int prev = -1, cur;
+//             q.push(i); // unvisited node
+
+//             while(q.size()){
+//                 cur = q.front();
+//                 q.pop();
+
+//                 visited[cur] = 1;
+
+//                 for(auto &it: adj[cur]){
+//                     if (it == prev) {
+//                         // skip
+//                     }
+//                     else {
+//                         if (visited[it]) return true;
+//                         else q.push(it);
+//                     }
+//                 }
+//                 prev = cur;
+//             }
+//         }
+//         return false;
+//     }
+// };
+
+// LogicalError: 
+// class Solution
+// {
+//     // DFS: Solution with stack: WA: How to track the previous state.
+// public:
+//     bool isCycle(int V, vector<int> adj[])
+//     {
+//         vector<int> visited(V);
+//         stack<int> st;
+//         for (int i = 0; i < V; ++i)
+//         {
+//             if (!visited[i])
+//             {
+//                 st.push(i);
+//                 while (st.size())
+//                 {
+//                     int top = st.top();
+//                     st.pop();
+
+//                     if (visited[top]) return true;
+//                     visited[top] = 1;
+
+//                     for (auto &it : adj[top]) {st.push(it);}
+//                 }
+//             }
+//         }
+//         return false;
+//     }
+// };
 
 class Solution
 {
