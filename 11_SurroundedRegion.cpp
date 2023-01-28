1812320e8f09e38509521d2a2bf5f143dfe2ef5a
Shubhansu kumar <shubhansu2021@gmail.com>
2023-01-28 15:28:22 +0530

    Solution1: BFS
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- 11_SurroundedRegion.cpp
+++ 11_SurroundedRegion.cpp
@@ -7,6 +7,72 @@
 #include <bits/stdc++.h>
 using namespace std ;
 
+class Solution1 {
+    // BFS: Traverse the boundry O's and flood all the connected zeroes.
+public:
+    void solve(vector<vector<char>>& board) {
+        int m = board.size(), n = board[0].size();
+        vector<vector<int>> visited(m, vector<int>(n));
+
+        vector<vector<char>> ans(m, vector<char>(n, 'X'));
+        queue<pair<int, int>> q;
+
+        for (int j = 0; j < n; ++j)
+        {
+            if (board[0][j] == 'O') {
+                visited[0][j] = 1;
+                q.push({0, j});
+            } 
+            if (board.size() > 1) {
+                if (board[m-1][j] == 'O'){
+                    visited[m-1][j] = 1;
+                    q.push({m-1, j});
+                }
+            }
+        }
+
+        for (int i = 1; i < m-1; ++i)
+        {
+            if (board[i][0] == 'O') {
+                visited[i][0] = 1;
+                q.push({i, 0});
+            } 
+            if (board.size() > 1) {
+                if (board[i][n-1] == 'O'){
+                    visited[i][n-1] = 1;
+                    q.push({i, n-1});
+                }
+            }
+        }
+        
+        while(q.size()){
+            pair<int, int> p = q.front();
+            q.pop();
+
+            int r = p.first, c = p.second, nRow, nCol;
+            ans[r][c] = 'O';
+
+            vector<int> rowShift = {-1, 0, +1, 0}, colShift = {0, -1, 0, +1};
+
+            for (int i = 0; i < 4; ++i)
+            {
+                nRow = r + rowShift[i];
+                nCol = c + colShift[i];
+
+                if (nRow >= 0 && nCol >= 0 && nRow < m && nCol < n){
+                    if (board[nRow][nCol] == 'O' && !visited[nRow][nCol]){
+                        q.push({nRow, nCol});
+                        visited[nRow][nCol] = 1;
+                    }
+                }
+            }             
+        }
+
+        board = ans;
+        // return ans;
+    }
+};
+
 class Solution {
     // DFS: Traverse the boundry O's and flood all the connected zeroes.
 public:
