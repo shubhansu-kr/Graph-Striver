9064ddaedf7c9959010903bdf0c7393353c740a0
Shubhansu kumar <shubhansu2021@gmail.com>
2023-02-13 10:28:24 +0530

    Detect Cycle in Directed Graph: Check if we can take all the course
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- 15_DetectCycleDirectedGraph.cpp
+++ 15_DetectCycleDirectedGraph.cpp
@@ -4,6 +4,40 @@
 using namespace std ;
 
 class Solution {
+    // DFS : Use Path vector to track the visited path node instead of set. 
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(numCourses+1);
+        for(auto &it: prerequisites){
+            adj[it[1]].emplace_back(it[0]);
+        }
+
+        vector<int> vis(numCourses), path(numCourses);
+        for (int i = 0; i < numCourses; ++i)
+        {
+            if (!vis[i] && dfs(numCourses, adj, vis, path, i)) return false;
+        }
+        
+        return true;
+    }
+
+    bool dfs(int &n, vector<vector<int>> &adj, vector<int> &vis, vector<int> &path, int &i){
+        if (path[i]) return true;
+        if (vis[i]) return false;
+
+        vis[i] = 1;
+        path[i] = 1;
+
+        for(int &it: adj[i]) {
+            if (dfs(n, adj, vis, path, it)) return true;   
+        }
+        path[i] = 0;
+
+        return false;
+    }
+};
+
+class Solution {
     // TLE
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
