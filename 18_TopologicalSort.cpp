057820dbc2b9831374a774bd3fc4675ad4c0c342
Shubhansu kumar <shubhansu2021@gmail.com>
2023-02-17 15:40:37 +0530

    Topological sort using dfs
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

---
+++ 18_TopologicalSort.cpp
@@ -0,0 +1,62 @@
+// Topological Sort
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+class Solution
+{
+    // Using dfs
+public: 
+    vector<int> topologicalSort(vector<vector<int>> &edges, int &n) {
+        // Create adj list 
+        vector<vector<int>> adj(n);
+        for(auto &it: edges) {
+            adj[it[0]].emplace_back(it[1]);
+        }
+        
+        vector<int> vis(n);
+        stack<int> temp;
+
+        for (int i = 0; i < n; ++i)
+        {
+            if (vis[i]) continue;
+            dfs(adj, temp, vis, i);
+        }
+
+        vector<int> ans(n);
+        for (int i = 0; i < n; ++i)
+        {
+            ans[i] = temp.top();
+            temp.pop();
+        }
+        
+        return ans;
+    }
+
+    void dfs(vector<vector<int>> &adj, stack<int> &temp, vector<int> &vis, int i){
+        stack<int> st;
+
+        st.push(i);
+        vis[i] = 1;
+
+        while(st.size()) {
+            int top = st.top();
+            st.pop();
+
+            temp.push(top);
+
+            for(auto &it: adj[top]) {
+                if (vis[it]) continue;
+                st.push(it);
+                vis[it] = 1;
+            }
+        }
+
+        return;
+    }
+};
+
+int main () {
+    
+    return 0;
+}
