ad00d9bb97e08bf6ea491652594a296cc3e74623
Shubhansu kumar <shubhansu2021@gmail.com>
2023-02-17 15:43:07 +0530

    Solution1: Topological Sort using bfs
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- 18_TopologicalSort.cpp
+++ 18_TopologicalSort.cpp
@@ -5,6 +5,59 @@ using namespace std ;
 
 class Solution
 {
+    // Using bfs
+public: 
+    vector<int> topologicalSort(vector<vector<int>> &edges, int &n) {
+        // Create adj list 
+        vector<vector<int>> adj(n);
+        for(auto &it: edges) {
+            adj[it[0]].emplace_back(it[1]);
+        }
+        
+        vector<int> vis(n);
+        stack<int> temp;
+
+        for (int i = 0; i < n; ++i)
+        {
+            if (vis[i]) continue;
+            bfs(adj, temp, vis, i);
+        }
+
+        vector<int> ans(n);
+        for (int i = 0; i < n; ++i)
+        {
+            ans[i] = temp.top();
+            temp.pop();
+        }
+        
+        return ans;
+    }
+
+    void bfs(vector<vector<int>> &adj, stack<int> &temp, vector<int> &vis, int i){
+        queue<int> q;
+
+        q.push(i);
+        vis[i] = 1;
+
+        while(q.size()) {
+            int top = q.front();
+            q.pop();
+
+            temp.push(top);
+
+            for(auto &it: adj[top]) {
+                if (vis[it]) continue;
+                q.push(it);
+                vis[it] = 1;
+            }
+        }
+
+        return;
+    }
+};
+
+class Solution
+{
     // Using dfs
 public: 
     vector<int> topologicalSort(vector<vector<int>> &edges, int &n) {
